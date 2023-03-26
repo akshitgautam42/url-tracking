da37bfa814e400ed2eb28100ccd84b8df00ba4d4
akshitgautam42 <bittugautam1224@gmail.com>
2023-03-26 14:20:50 +0530

    View added

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const app = express();
 const port = 3000;
 
 // Mount the API route at /api
-app.use('/api', api);
-
+//app.use('/api', api);
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
