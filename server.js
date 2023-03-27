b8051b74960fc98e82acf2f1baf6b2ec02e9aff9
akshitgautam42 <bittugautam1224@gmail.com>
2023-03-27 15:44:45 +0530

    shorURL model added

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const db = require('./db');
 const api = require('./api');
+const ShortURL = require('/models/shortURL');
 
 const app = express();
 const port = 3000;
+
+mongoose.connect('mongodb')
 
 // Mount the API route at /api
 //app.use('/api', api);
